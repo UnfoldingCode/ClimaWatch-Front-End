105564a2af1c7ab160a90b0713db6480d31bbba9
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-30 11:41:51 -0400

    Grab city

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let city = document.querySelector("#city");
+
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e572668bb21fee7042efec77137cc15c`;
 let search_button = document.querySelector("#search_button");
 search_button.addEventListener("click", fetch_weather);
@@ -14,6 +16,7 @@ async function fetch_weather() {
   let res = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=kathmandu&units=metric&appid=e572668bb21fee7042efec77137cc15c`
   );
+  console.log(city.value);
 
   console.log(res);
   let data = await res.json();
