55c70f88f3cb1f8f70e559296b7ddc369ba11d90
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-30 13:05:59 -0400

    button click sets the api value

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let search_button = document.querySelector("#search_button");
+let api;
+
+search_button.addEventListener("click", () => {
+  let city = document.querySelector("#city").value;
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e572668bb21fee7042efec77137cc15c`;
+});
+
 search_button.addEventListener("click", fetch_weather);
 
 async function fetch_weather() {
-  let city = document.querySelector("#city").value;
-  let res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e572668bb21fee7042efec77137cc15c`
-  );
+  let res = await fetch(api);
   console.log(res);
   let data = await res.json();
   console.log(data.sys.country);
